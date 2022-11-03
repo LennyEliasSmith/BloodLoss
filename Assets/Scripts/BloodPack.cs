d4435898dd4e6c811ccd14052154ef4cad6d20dc
Lenny Smith <lenny.elias.s@gmail.com>
2022-11-03 23:56:09 +0200

    Implemented Blood pack pickup

---
+++ Assets/Scripts/BloodPack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Main.Game
+{
+    public class BloodPack : Pickup
+    {
+        public enum BloodType
+        {
+            SMALL,
+            MEDIUM,
+            LARGE
+        }
+
+        public BloodType type;
+
+        [HideInInspector] public float bloodAmount;
+
+        [SerializeField] private float smallBloodAmount;
+        [SerializeField] private float mediumBloodAmount;
+        [SerializeField] private float largeBloodAmount;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            switch (type)
+            {
+                case BloodType.SMALL:
+                    bloodAmount = smallBloodAmount;
+                    break;
+                case BloodType.MEDIUM:
+                    bloodAmount = mediumBloodAmount;
+                    break;
+                case BloodType.LARGE:
+                    bloodAmount = largeBloodAmount;
+                    break;
+
+            }
+        }
+
+        protected override void OnPickedUp(Blood blood)
+        {
+            Blood playerBlood = blood.GetComponent<Blood>();
+
+            playerBlood.currentBlood += bloodAmount;
+
+            Destroy(gameObject);
+        }
+
+    }
+}
+
