731691c67c62c343235ea3632659f6e16fe644df
Lenny Smith <lenny.elias.s@gmail.com>
2022-11-06 18:43:16 +0200

    Misc Changes

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,13 @@
+
+namespace Main.Game
+{
+    public class GameConstants
+    {
+        public const string VerticalAxis = "Vertical";
+        public const string HorizontalAxis = "Horizontal";
+
+        public const string MouseX = "Mouse X";
+        public const string MouseY = "Mouse Y";
+    }
+}
+
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,3 +1,4 @@
+using Main.Game;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -11,6 +12,9 @@ public class PlayerMover : MonoBehaviour
     public float rotationSpeed;
     public float innertia;
     public float mass;
+    public float howfast;
+    public Vector3 playerVelocity;
+    private Vector3 previosPosition;
 
     private Rigidbody rb;
     private Camera cam;
@@ -24,19 +28,27 @@ public class PlayerMover : MonoBehaviour
 
     void FixedUpdate()
     {
+        playerVelocity = (transform.position - previosPosition) / Time.deltaTime;
+        howfast = playerVelocity.magnitude;
+        Mover();
+        previosPosition = transform.position;
+    }
+
+
+    void Mover()
+    {
         //Setting axis
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw(GameConstants.HorizontalAxis);
+        float vertical = Input.GetAxisRaw(GameConstants.VerticalAxis);
 
 
         // Creating movement vector and normalizing it a.k.a making things smooth
-        Vector3 movementVector = new Vector3(horizontal,0,vertical);
+        Vector3 movementVector = new Vector3(horizontal, 0, vertical);
         movementVector = movementVector.normalized * speed * Time.fixedDeltaTime;
 
         Vector3 direction = cam.transform.rotation * movementVector;
 
         //Moving the rigidbody
-        rb.MovePosition(rb.position + new Vector3(direction.x,0, direction.z));
-       
+        rb.MovePosition(rb.position + new Vector3(direction.x, 0, direction.z));
     }
 }
